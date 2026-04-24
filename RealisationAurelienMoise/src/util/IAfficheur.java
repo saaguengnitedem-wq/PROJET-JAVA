3c7ab86240c9187a37a1335cd6238139b7cbffb9
Aurelien Martial <saague.ngnitedem@gmail.com>
2026-04-23 20:54:22 -0400

    Remove Javadoc comment from IAfficheur interface
    Removed the Javadoc comment explaining the IAfficheur interface.

--- RealisationAurelienMoise/src/util/IAfficheur.java
+++ RealisationAurelienMoise/src/util/IAfficheur.java
@@ -5,18 +5,7 @@ import service.GestionnaireEtudiants;
 
 import java.util.List;
 
-/**
- * INTERFACE définissant le contrat de tout afficheur de résultats.
- *
- * <p>L'interface permet de brancher facilement un autre type
- * d'affichage (ex : {@code AfficheurHTML}, {@code AfficheurGraphique},
- * {@code AfficheurSilencieux} pour les tests) sans toucher ni au
- * modèle, ni à la logique métier, ni à Main (hormis une ligne
- * d'instanciation).
- *
- * @author Aurelien et Moise
- * @version 3.0
- */
+
 public interface IAfficheur {
 
     /** Affiche un titre encadré. */
